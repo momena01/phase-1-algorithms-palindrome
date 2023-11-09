789be2e70574e2967ced724eed50eebdff69a649
momena <momena.salloum59@gmail.com>
2023-11-09 17:01:53 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    let newWord = word.split('').reverse().join('');
+    if (word == newWord) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-
 /* 
   Add your pseudocode here
+  read word- reverce word as a new one - if newWord == word - return T - else - return F 
 */
 
 /*
@@ -12,14 +19,17 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+    console.log("=>", isPalindrome("madam"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+    console.log("=>", isPalindrome("memo"));
+
 }
 
 module.exports = isPalindrome;
